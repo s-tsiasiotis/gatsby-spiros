18d2ce42a0171d8d9ff92e6b23310f0a7c505a29
s-tsiasiotis <spirostsiasiotis@gmail.com>
2021-03-19 16:51:35 +0100

    favicon and primary color

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,10 +22,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#4169e1`,
+        theme_color: `#4169e1`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/st_logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
